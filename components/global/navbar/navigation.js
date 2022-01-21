d78cde1786d53e851923268eccbe9fe81574c0d3
dityath <dityaathallah107@gmail.com>
2022-01-21 15:51:15 +0700

    add login button

--- components/global/navbar/navigation.js
+++ components/global/navbar/navigation.js
@@ -2,5 +2,5 @@ export const navs = [
   { text: "Beranda", href: "/" },
   { text: "Katalog", href: "/#katalog" },
   { text: "Register", href: "/register" },
-  { text: "Login", href: "https://www.google.com/" },
+  { text: "Login", href: "https://dashboard-nurul-qolbi.vercel.app/login" },
 ];
