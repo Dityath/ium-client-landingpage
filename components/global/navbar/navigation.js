b716c9f1c1a3b096451d908d3d36abc6922b4e08
dityath <dityaathallah107@gmail.com>
2022-01-22 14:14:54 +0700

    jadi

--- components/global/navbar/navigation.js
+++ components/global/navbar/navigation.js
@@ -2,5 +2,8 @@ export const navs = [
   { text: "Beranda", href: "/" },
   { text: "Katalog", href: "/#katalog" },
   { text: "Register", href: "/register" },
-  { text: "Login", href: "https://dashboard-nurul-qolbi.vercel.app/login" },
+  {
+    text: "Login",
+    href: "https://ium-client-dashboard-9n9c0zz2s-dityath.vercel.app/login",
+  },
 ];
